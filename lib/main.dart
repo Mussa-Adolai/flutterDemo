79e94bbcefa519bcbf94c98aabd629caa4c1979b
Mussa-Adolai <60032727+Mussa-Adolai@users.noreply.github.com>
2022-05-23 23:21:00 +0300

    flutter 2

--- lib/main.dart
+++ lib/main.dart
@@ -7,11 +7,14 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.blue,
         appBar: AppBar(
-            title: Text('I am Rich'), backgroundColor: Colors.blueGrey[900]),
+          title: Text('I am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
         body: Center(
           child: Image(
-            image: NetworkImage(
-                'https://www.bing.com/th?id=OIP.tF0w4g4aOVgm1osTQT_8PwHaDt&w=203&h=101&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2'),
+            image: AssetImage(
+              'Images/diamond1.png',
+            ),
           ),
         ),
       ),
